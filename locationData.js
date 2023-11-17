fa90d003fbf4b27446a79d04098078b8f22c4752
MrRobit <armaniefel98@gmail.com>
2023-11-16 22:15:37 -0500

    got parks and mountains dropdowns to function justo need to style and create read me

---
+++ locationData.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+
+// let selectedStateEl = document.getElementById("states");
+
+// locationsArray.forEach(location => {
+//     let option = document.createElement("option");
+//     option.value = location;
+//     option.text = location;
+//     selectedStateEl.appendChild(option);
+// }); 
+
+
